7db2ec7926e632b84290772953fe446877028b0a
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-10-03 03:24:01 +0800

    添加馆藏查询功能

---
+++ pages/collection/collection.js
@@ -0,0 +1,184 @@
+// collection.js
+//获取应用实例
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [],
+    bookname: null,
+    currentPage: 0,
+    sumPage: 0,
+    hasMore: false,
+    loading: false
+  },
+  formSubmit: function(e) {
+    const page = this
+    let bookname = e.detail.value.bookname
+    if (!bookname || bookname.length == 0) {
+      this.showTopTips("请填写要查询的书名")
+    } else {
+      let requestData = {
+        bookname: bookname,
+        page: 1
+      }
+      this.setData({
+        loading: true
+      })
+      wx.showNavigationBarLoading()
+      wx.request({
+        url: "https://www.gdeiassistant.cn/rest/collectionquery",
+        method: "POST",
+        header: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data: requestData,
+        success: function(result) {
+          page.setData({
+            loading: false
+          })
+          wx.hideNavigationBarLoading()
+          if (result.data.success) {
+            if (result.data.collectionList.length == 0) {
+              page.showTopTips("没有找到对应的图书信息")
+            } else {
+              let list = page.data.list
+              result.data.collectionList.forEach(function(e) {
+                list.push(e)
+              })
+              page.setData({
+                bookname: bookname,
+                list: list,
+                currentPage: 1,
+                sumPage: result.data.sumPage
+              })
+            }
+          } else {
+            page.showTopTips(result.data.errorMessage)
+          }
+        },
+        fail: function() {
+          page.setData({
+            loading: false
+          })
+          wx.hideNavigationBarLoading()
+          page.showTopTips("网络连接超时，请重试")
+        }
+      })
+    }
+  },
+  loadMore: function() {
+    let page = this
+    wx.showLoading({
+      title: '数据加载中',
+      mask: true
+    });
+    let requestData = {
+      bookname: this.data.bookname,
+      page: this.data.currentPage + 1
+    }
+    wx.request({
+      url: "https://www.gdeiassistant.cn/rest/collectionquery",
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data: requestData,
+      success: function(result) {
+        if (result.data.success) {
+          if (result.data.collectionList.length == 0) {
+            page.showTopTips("没有更多图书信息")
+            wx.hideLoading()
+          } else {
+            let list = page.data.list
+            result.data.collectionList.forEach(function(e) {
+              list.push(e)
+            })
+            page.setData({
+              list: list,
+              currentPage: page.data.currentPage + 1
+            })
+            wx.hideLoading()
+          }
+        } else {
+          page.showTopTips(result.data.errorMessage)
+          wx.hideLoading()
+        }
+      },
+      fail: function() {
+        page.showTopTips("网络连接超时，请重试")
+        wx.hideLoading()
+      }
+    })
+  },
+  showTopTips: function(content) {
+    var that = this;
+    this.setData({
+      errorMessage: content
+    });
+    setTimeout(function() {
+      that.setData({
+        errorMessage: null
+      });
+    }, 3000);
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
---
+++ pages/collectionDetail/collectionDetail.js
@@ -0,0 +1,127 @@
+// pages/collectionDetail/collectionDetail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    query: null,
+    result: null
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let page = this
+    wx.setNavigationBarTitle({
+      title: options.bookname
+    })
+    this.setData({
+      query: {
+        opacUrl: options.opacUrl,
+        page: options.page,
+        schoolId: options.schoolId,
+        search: options.search,
+        searchtype: options.searchtype,
+        xc: options.xc
+      }
+    })
+    wx.showNavigationBarLoading()
+    wx.request({
+      url: "https://www.gdeiassistant.cn/rest/collectiondetail",
+      method: "POST",
+      data: this.data.query,
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      success: function (result) {
+        wx.hideNavigationBarLoading()
+        if (result.data.success) {
+          let detail = result.data.data
+          if (detail.principal.split(" ").length != 1){
+            detail.autograph = detail.principal.split(" ")[0]
+            detail.director = detail.principal.split(" ")[1]
+          }
+          if (detail.publishingHouse.split(" ").length!=1){
+            detail.house = detail.publishingHouse.split(" ")[0]
+            detail.year = detail.publishingHouse.split(" ")[1]
+          }
+          if (detail.price.split(" ").length != 1){
+            detail.ISBN = detail.price.split(" ")[0]
+            detail.priceValue = detail.price.split(" ")[1]
+          }
+          page.setData({
+            result: detail
+          })
+        } else {
+          page.showTopTips(result.data.errorMessage)
+        }
+      },
+      fail: function () {
+        wx.hideNavigationBarLoading()
+        page.showTopTips("网络连接超时")
+      }
+    })
+  },
+  showTopTips: function (content) {
+    var that = this;
+    this.setData({
+      errorMessage: content
+    });
+    setTimeout(function () {
+      that.setData({
+        errorMessage: null
+      });
+    }, 3000);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
