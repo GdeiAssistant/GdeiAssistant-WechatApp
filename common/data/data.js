c7e824c69df550e03bc884b1c944ccfbec27042a
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-02-25 18:45:34 +0800

    配置小程序版本号和防重放攻击校验令牌

---
+++ common/data/data.js
@@ -0,0 +1,10 @@
+//防重放攻击校验令牌
+const requestValidateToken = "Replace with your request token"
+
+//小程序版本号
+const versionCode = "Replace with your version code"
+
+module.exports = {
+  requestValidateToken: requestValidateToken,
+  versionCode: versionCode
+}
