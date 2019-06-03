4d3044e26a2198379d7e8b670a3c80a10863a642
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-06-04 03:33:46 +0800

    提供用于体验预览的生产环境默认请求令牌值

--- common/data/data.js
+++ common/data/data.js
@@ -1,5 +1,5 @@
-//防重放攻击校验令牌
-const requestValidateToken = "Replace with your request token"
+//防重放攻击校验令牌，该默认值为生产环境供开发者体验预览的令牌值
+const requestValidateToken = "GdeiAssistantRequest"
 
 //小程序版本号
 const versionCode = "Replace with your version code"
